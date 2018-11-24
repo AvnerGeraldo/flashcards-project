da18a7c9bd8fa6f1df43ebe5fefcd419f97c0976
Avner <avner.conceicao@hotmail.com>
2018-11-24 20:56:59 +0000

    Criando App de entrada

--- App.js
+++ App.js
@@ -4,13 +4,10 @@ import { Provider } from 'react-redux'
 //Store
 import store from '@application/store'
 //Components
-import Container from '@styled-components/Container'
 import Application from '@application'
 
 export default () => (
   <Provider store={store}>
-    <Container>
-      <Application />
-    </Container>
+    <Application />
   </Provider>
 )
--- application/index.js
+++ application/index.js
@@ -1,36 +1,25 @@
-import React from 'react';
-import { Text } from 'react-native'
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux'
 import Container from '@styled-components/Container'
 
-//API
-import * as api from '@api'
+//Actions
+import { SET_INITIAL_DATA } from '@actions/saga-actions'
 
 //Routes
 import Route from '@routes'
 
-//api.setInitialData().then(res => console.log(res))
-/*
-api.getDecks().then(res => console.log(res))
-api.getDeck('JavaScript').then(res => console.log(res))
-*/
-//api.saveDeckTitle('Teste')//.then(res => console.log(res))
-//.then(res => api.getDecks().then(res => console.log(res)))
-/*api.saveDeckTitle('Teste')
-  .then(res => 
-      api.addCardToDeck('Teste', {
-        question: 'What is React?2222',
-        answer: 'A library for managing useraaaaa interfaces'
-      }
-    )
-    .then(res => 
-      api.getDecks()
-        .then(res => console.log(res))
-    )
-  )
-  */
+class Application extends PureComponent {
+  componentDidMount() {
+    this.props.dispatch({ type: SET_INITIAL_DATA })    
+  }
 
-export default () => (
-  <Container>
-    <Route />
-  </Container>
-)
+  render() {
+    return (
+      <Container>
+        <Route />
+      </Container>
+    )
+  }
+}
+
+export default connect(null)(Application)
