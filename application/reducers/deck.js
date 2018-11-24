b84ac86e8c8861a2e016556e99538cdcd4dff557
Avner <avner.conceicao@hotmail.com>
2018-11-24 20:47:07 +0000

    Criando reducers da aplicação

--- application/reducers/deck.js
+++ application/reducers/deck.js
@@ -1,16 +1,28 @@
-import { handleActions } from 'redux-actions'
+import { GET_DECKS_ASYNC, ERROR_DECKS_ASYNC } from '@actions'
 
-export default handleActions({
-        GET_DECKS: (state, { dataDecks }) => ({
-            ...state,
-            dataDecks,
-        }),
-        ERROR: (state, { error }) => ({
-            ...state,
-            error
-        })
+const initialState = {
+    dataDecks: {},
+    error: ''
+}
+
+export default (state = initialState, { type, payload }) => {
+    switch(type) {
+        case GET_DECKS_ASYNC:
+            const { dataDecks } = payload
+            return {
+                ...state,
+                dataDecks
+            }
+        case ERROR_DECKS_ASYNC: {
+            const { error } = payload
+            return {
+                ...state,
+                dataDecks: {},
+                error
+            }
+        }
+
+        default:
+            return state
     }
-    , {
-        dataDecks: {},
-        error: ''
-})
+}
--- application/reducers/playCards.js
+++ application/reducers/playCards.js
@@ -1,16 +1,25 @@
-import { handleActions } from 'redux-actions'
+import { GET_DECK_QUESTIONS_ASYNC, ERROR_QUESTIONS_ASYNC } from '@actions'
 
-export default handleActions({
-    GET_DECK_QUESTIONS: (state, { deck }) => ({
-        ...state,
-        deck,
-    }),
-    ERROR: (state, { error }) => ({
-        ...state,
-        error
-    })
-}
-, {
+const initialState = {
     questions: [],
     deck: ''
-})
+}
+
+export default (state = initialState, { type, payload }) => {
+    switch(type) {
+        case GET_DECK_QUESTIONS_ASYNC:
+            const { deck } = payload
+            return {
+                ...state,
+                deck
+            }
+        case ERROR_QUESTIONS_ASYNC:
+            const { error } = payload
+            return {
+                ...state,
+                error
+            }
+        default:
+            return state
+    }
+}
