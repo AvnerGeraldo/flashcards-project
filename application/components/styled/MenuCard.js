ea60d3bb0d0a5dacafaa4d883b32c0e9505535c4
Avner <avner.conceicao@hotmail.com>
2018-11-25 13:28:16 +0000

    Add ao 'MenuCard' buttons de 'Add Card' e 'Start Quiz'

--- application/components/styled/MenuCard.js
+++ application/components/styled/MenuCard.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Ionicons } from '@expo/vector-icons'
+import PropTypes from 'prop-types'
 
 //Components
 import Card from './Card'
+import ContainerButton from './ContainerCard'
+import ButtonCard from './ButtonCard'
+import ButtonText from './ButtonText'
 
-const MenuCard = styled.View`
+const ContainerMenuCard = styled.View`
     flex: 1;
     flex-direction: row;
     justify-content: center;
@@ -14,14 +17,39 @@ const MenuCard = styled.View`
 `
 
 const Title = styled.Text`
-    color: #FFFF00;
-    font-size: 16;
+    font-size: 24px;
+`
+const SubTitle = styled.Text`
+    font-size: 15px;
+    color: #FF4500;
 `
 
-export default ({ title, nameIcon }) => (
-    <MenuCard>
-        <Card>
-            <Title>{title}</Title>
-        </Card>
-    </MenuCard>
-)
+const MenuCard = ({ title, questions }) => {
+    const counterQuestions = questions.length
+
+    return (
+        <ContainerMenuCard>
+            <Card>
+                <Title>{title}</Title>
+                <SubTitle>{counterQuestions} Cards</SubTitle>
+                <ContainerButton>
+                    <ButtonCard>
+                        <ButtonText>Add Card</ButtonText>
+                    </ButtonCard>
+                    <ButtonCard backgroundColor='#000'>
+                        <ButtonText colorText='#FFF'>Start Quiz</ButtonText>
+                    </ButtonCard>
+                </ContainerButton>                
+            </Card>
+        </ContainerMenuCard>
+    )
+}
+
+const { string, array } = PropTypes
+
+MenuCard.propTypes = {
+    title: string.isRequired,
+    questions: array.isRequired
+}
+
+export default MenuCard;
