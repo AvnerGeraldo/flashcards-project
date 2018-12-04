3e93e9f15a97810575cae550189da42ea460c1b4
Avner <avner.conceicao@hotmail.com>
2018-12-04 23:26:39 +0000

    Removendo animações de FlipCard e deixando mais simples

--- application/components/FlipCard.js
+++ application/components/FlipCard.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Animated, Dimensions, StyleSheet, Button } from 'react-native'
 
 //Styled Components
 import ContainerQuiz from '@styled-components/ContainerQuiz'
@@ -7,96 +6,53 @@ import FrontCard from '@styled-components/FrontCard'
 import BackCard from '@styled-components/BackCard'
 
 class FlipCard extends Component {
-
-    componentWillMount() {
-        this.animatedValue = new Animated.Value(0)
-        this.value = 0
-        this.animatedValue.addListener(({ value }) => this.value = value)
-
-        this.frontInterpolate = this.animatedValue.interpolate({
-            inputRange: [0, 180],
-            outputRange: ['0deg', '180deg'],
-        })
-
-        this.backInterpolate = this.animatedValue.interpolate({
-            inputRange: [0, 180],
-            outputRange: ['180deg', '360deg'],
-        })
-
-        this.frontOpacity = this.animatedValue.interpolate({
-            inputRange: [89, 90],
-            outputRange: [1, 0]
-        })
-
-        this.backOpacity = this.animatedValue.interpolate({
-            inputRange: [89, 90],
-            outputRange: [0, 1]
-        })
+    state = {
+        showBackCard: false
     }
 
     flipCard = () => {
-        if (this.value >= 90) {
-            Animated.spring(this.animatedValue, {
-                toValue: 0,
-                friction: 8,
-                tension: 10
-            }).start()
-        } else {
-            Animated.spring(this.animatedValue, {
-                toValue: 180,
-                friction: 8,
-                tension: 10
-            }).start()
-        }
+        const { showBackCard } = this.state
+        this.setState({ showBackCard: !showBackCard })
+    }
+
+    completeCard = (answered) => {
+        this.props.receiveAnwser(answered);
+
+        //Virar o novo card para front
+        this.setState({
+            showBackCard: false
+        })
     }
 
     render() {
-        const frontAnimatedStyle = {
-            transform: [
-              { rotateY: this.frontInterpolate }
-            ]
-          }
-
-        const backAnimatedStyle = {
-            transform: [
-                { rotateY: this.backInterpolate }
-            ]
-        }
-
-        const { question, answer, positionCard, totalCountCards, receiveAnwser } = this.props
+        const { question, answer, positionCard, totalCountCards } = this.props
+        const { showBackCard } = this.state
 
         return (
             <ContainerQuiz>
-                <Animated.View style={[styles.flipCard, frontAnimatedStyle, {opacity: this.frontOpacity}]}>
-                    <FrontCard 
-                        actualPos={positionCard}
-                        endPos={totalCountCards} 
-                        question={question} 
-                        flipCard={this.flipCard}
-                    />
-                </Animated.View>
-                <Animated.View style={[styles.flipCard, styles.flipCardBack, backAnimatedStyle, {opacity: this.backOpacity}]}>                    
-                    <BackCard 
-                        actualPos={positionCard}
-                        endPos={totalCountCards}
-                        answer={answer}
-                        responseAnswer={receiveAnwser}
-                    />
-                </Animated.View>            
+                {
+                    !showBackCard 
+                    ? (
+                        <FrontCard 
+                            actualPos={positionCard}
+                            endPos={totalCountCards} 
+                            question={question} 
+                            flipCard={this.flipCard}
+                        />
+                    )
+                    :
+                    (
+                        <BackCard 
+                            actualPos={positionCard}
+                            endPos={totalCountCards}
+                            answer={answer}
+                            responseAnswer={this.completeCard}
+                        />
+                    )
+                }           
             </ContainerQuiz>
         )        
     }
 }
-
-const styles = StyleSheet.create({
-    flipCard: {
-        backfaceVisibility: 'hidden'        
-    },
-    flipCardBack: {      
-        position: 'absolute',
-        top: 0,
-        bottom: 0
-    },
-  });
 
 export default FlipCard
