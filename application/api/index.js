93bc7782526bbec232afed4b91afd4f0e4e9f98d
Avner <avner.conceicao@hotmail.com>
2018-12-05 00:07:00 +0000

    Criando método 'saveAnswerQuestion' da API

--- application/api/index.js
+++ application/api/index.js
@@ -80,11 +80,40 @@ export const addCardToDeck = async(title, card) => {
 
         //Exists the deck?
         if (!deck) {
-            throw new Exception('Error: Add card to deck.')
+            throw new Exception('Error: Deck not exists! Add card to deck.')
         }
         
         //Add Card to deck
         allData[title].questions.push(card)    
+    
+        //Set the new data on AsyncStorage
+        await AsyncStorage.setItem(token, JSON.stringify(allData))
+
+        //Return the new value
+        return { save: true }
+    } catch(error) {
+        return { error }
+    }
+}
+
+export const saveAnswerQuestion = async(title, indexQuestion, answered) => {
+    try {
+        //Get data API
+        const allData = await getDataOnAsyncStorage()
+
+        //Keys
+        const arrayAllDataKeys = Object.keys(allData)
+
+        //Get deck in API
+        const deck = arrayAllDataKeys.filter(titleOfDeck => titleOfDeck === title && allData[title])
+
+        //Exists the deck?
+        if (!deck) {
+            throw new Exception('Error: Deck not exists! Add card to deck.')
+        }
+        
+        //Add answer in question
+        allData[title].questions[indexQuestion].answered = answered
     
         //Set the new data on AsyncStorage
         await AsyncStorage.setItem(token, JSON.stringify(allData))
