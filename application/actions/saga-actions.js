a6c3170386bdfa3f1328fa6522dd5a18faa89f8a
Avner <avner.conceicao@hotmail.com>
2018-12-19 01:04:04 +0000

    Ajustando actions

--- application/actions/saga-actions.js
+++ application/actions/saga-actions.js
@@ -1,3 +1,4 @@
 export const SET_INITIAL_DATA = 'SET_INITIAL_DATA'
+export const GET_DECK = 'GET_DECK'
 export const GET_DECKS = 'GET_DECKS'
 export const SAVE_ANSWER_ON_DECK = 'SAVE_ANSWER_ON_DECK'
