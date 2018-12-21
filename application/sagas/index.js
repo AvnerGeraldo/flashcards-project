f3ce27ee60c40ff5385bcfc78222d8053243efb1
Avner <avner@barramansa.unimed.com.br>
2018-12-21 16:40:09 -0200

    Add 'watchSaveDeckTitle' ao middleware saga

--- application/sagas/index.js
+++ application/sagas/index.js
@@ -4,6 +4,7 @@ import { all } from 'redux-saga/effects'
 import watchInitialData from './Deck/getInitialData'
 import watchGetDecks from './Deck/getDecks'
 import watchGetDeck from './Deck/getDeck'
+import watchSaveDeckTitle from './Deck/saveDeck'
 
 //Answer
 import watchSaveAnswer from './Deck/saveAnswerOnDeck'
@@ -15,5 +16,6 @@ export default function* () {
         watchGetDecks(),
         watchGetDeck(),
         watchSaveAnswer(),
+        watchSaveDeckTitle(),
     ])
 }
