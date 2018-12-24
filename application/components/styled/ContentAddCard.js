a5bfecd493947aa0d583f181f1919b7bcd5b7d6e
Avner <avner@barramansa.unimed.com.br>
2018-12-24 10:31:34 -0200

    Criando styled-component 'ContentAddCard'

---
+++ application/components/styled/ContentAddCard.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const ContentAddCard = styled.View`
+    flex: 1;
+    justify-content: flex-start;
+    align-items: stretch;
+    padding: 15px 10px 0px;
+`
+
+export default ContentAddCard
