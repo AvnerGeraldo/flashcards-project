0c77cf5f2c17dee27a877c220dfb082d294a1930
Avner <avner.conceicao@hotmail.com>
2018-12-23 23:27:59 -0200

    Removendo 'ContainerMenuCard' de 'MenuCard' e substituindo por View para resolver problema de FlatList

--- application/components/styled/MenuCard.js
+++ application/components/styled/MenuCard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dimensions } from 'react-native'
+import { Dimensions, View, Text } from 'react-native'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import { withNavigation } from 'react-navigation'
@@ -9,14 +9,15 @@ import Card from './Card'
 import ContainerButton from './ContainerButton'
 import ButtonCard from './ButtonCard'
 import ButtonText from './ButtonText'
-
+/*
 const ContainerMenuCard = styled.View`
     flex: 1;
+    width: ${Dimensions.get('window').width};
     justify-content: center;
     align-items: stretch;  
-    background-color: #ADD8E6;
-    width: ${Dimensions.get('window').width};
+    background-color: #ADD8E6;    
 `
+*/
 
 const ContentCard = styled.View`
     align-items: center;
@@ -34,7 +35,13 @@ const MenuCard = ({ navigation, title, questions }) => {
     const counterQuestions = questions.length
 
     return (
-        <ContainerMenuCard>
+        <View style={{
+            flex: 1,
+            width: Dimensions.get('window').width,
+            justifyContent: 'center',
+            alignItems: 'stretch',
+            backgroundColor: '#ADD8E6',
+        }}>
             <Card>
                 <ContentCard>
                     <Title>{title}</Title>
@@ -55,7 +62,7 @@ const MenuCard = ({ navigation, title, questions }) => {
                     </ContainerButton>
                 </ContentCard>               
             </Card>
-        </ContainerMenuCard>
+        </View>
     )
 }
 
