23946b32dc4c2ec94e888abe3954d74c7cc258dc
Avner <avner.conceicao@hotmail.com>
2018-12-26 20:19:03 -0200

    Ajustando saga 'saveDeck' para tratar os erros

--- application/sagas/Deck/saveDeck.js
+++ application/sagas/Deck/saveDeck.js
@@ -1,23 +1,28 @@
-import { SAVE_DECK, GET_DECKS, ERROR_DECKS_ASYNC } from '@actions/saga-actions'
+import { SAVE_DECK_ASYNC, ERROR_DECKS_ASYNC } from '@actions'
+import { SAVE_DECK } from '@actions/saga-actions'
 import { takeLatest, put } from 'redux-saga/effects'
 import { saveDeckTitle } from '@api'
 
 function* saveDeck({ payload: { resolve, reject, title }}) {
     try {
         const response = yield saveDeckTitle(title)
-
+        
         if (response.error) {
-            throw new response.error
+            throw new Error(response.error)
         }
 
         if (!response.save) {
-            throw new Error('Erro: Não foi possível salvar o deck. Tente novamente mais tarde!')
+            throw new Error('Error: Could not save the deck. Please try again another time!')
         }
         
         //Response data
+        yield put({
+            type: SAVE_DECK_ASYNC
+        })
+        
         resolve()
     } catch(error) {
-        put({
+        yield put({
             type: ERROR_DECKS_ASYNC,
             payload: {
                 error: error.message
