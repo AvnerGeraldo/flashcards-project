8a11bd5384346bf591d2d4fdea307c5eaf6e53dc
Avner <avner.conceicao@hotmail.com>
2018-12-26 21:39:42 -0200

    Add action 'SAVE_QUESTION_ON_DECK_ASYNC' ao salvar em 'saveQuestionOnDeck'

--- application/sagas/Deck/saveQuestionOnDeck.js
+++ application/sagas/Deck/saveQuestionOnDeck.js
@@ -1,5 +1,6 @@
+import { SAVE_QUESTION_ON_DECK_ASYNC, ERROR_DECK_ASYNC } from '@actions'
 import { SAVE_QUESTION_ON_DECK } from '@actions/saga-actions'
-import { takeLatest } from 'redux-saga/effects'
+import { takeLatest, put } from 'redux-saga/effects'
 import { addCardToDeck } from '@api'
 
 function* saveQuestion({ payload: { resolve, reject, title, question, answer }}) {
@@ -7,18 +8,27 @@ function* saveQuestion({ payload: { resolve, reject, title, question, answer }})
         const response = yield addCardToDeck(title, { question, answer })
 
         if (response.error) {
-            throw new response.error
+            throw new Error(response.error)
         }
 
         if (!response.save) {
-            throw new Error('Error: Could not save the question. Please try again later!')
+            throw new Error('Could not save the question. Please try again later!')
         }
-        
+        //Response
+        yield put({
+            type: SAVE_QUESTION_ON_DECK_ASYNC
+        })
+
         resolve()
-        return { save: true }
     } catch(error) {
-        resolve()
-        return { error: error.message }        
+        yield put({
+            type: ERROR_DECK_ASYNC,
+            payload: {
+                error: error.message
+            }
+        })
+
+        resolve()   
     }
 }
 
