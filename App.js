19ccc7728f02acd216d8d5165ebac7954be6a3ea
Avner <avner.conceicao@hotmail.com>
2018-12-27 21:59:26 -0200

    Setando notificações locais para app

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux'
 
 //Store
 import store from '@application/store'
+
 //Components
 import Application from '@application'
 
-export default () => (
-  <Provider store={store}>
-    <Application />
-  </Provider>
-)
+//Notifications
+import { setLocalNotifications } from '@utils/notifications'
+
+class App extends Component {
+    componentDidMount() {
+        setLocalNotifications()
+    }
+
+    render() {
+        return (
+            <Provider store={store}>
+              <Application />
+            </Provider>
+        )
+    }
+}
+
+export default App
